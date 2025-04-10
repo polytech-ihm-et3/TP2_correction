39776cf104a1cb63d438168a0546e32ed86c0b89
kunchtler <leo.kulinski@gmail.com>
2025-04-10 17:42:37 +0200

    Renamed smaple.fxml for consistency with squelette

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         primaryStage.setTitle("File selector");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
